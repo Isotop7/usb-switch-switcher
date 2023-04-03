6544bf906d9eed12c62c1d7a7d2dea8c762df013
Hendrik <hendrik@hr94.de>
2023-04-03 21:16:06 +0200

    rename source to target update README

--- README.md
+++ README.md
@@ -1,6 +1,88 @@
 # usb-switch-switcher
 
-## Homebridge
+We combine a small Wemos D1 Mini and a dumb UGREEN usb switch to control the selected usb target device via mqtt.
 
-### Mqttthing
+## hardware
 
+We use the UGREEN 2 to 4 USB 3.0 switch: [https://de.ugreen.com/products/ugreen-usb-switch](https://de.ugreen.com/products/ugreen-usb-switch-2pc-usb-3-0-switcher-2-in-4-out-usb-kvm-switch-mit-2-usb-3-0-a-auf-a-kabeln-fur-tastatur-maus-drucker-scanner-usb-sticks-usw) and a Wemos D1 mini. Any other esp8266-based microcontroller with wifi should work too.
+We also need a KY-019 relay like this one: [https://www.amazon.de/ARCELI-ST%C3%9CCKE-KY-019-Relaismodul-arduino](https://www.amazon.de/ARCELI-ST%C3%9CCKE-KY-019-Relaismodul-arduino).
+
+The hardware is connected like this:
+
+```
+ESP:
+ESP8266 Pin D1 -> Signal line of relay
+ESP8266 ground pin -> ground rail
+ESP8266 5V in -> 5V rail
+
+RELAY:
+VCC -> 5V rail
+ground pin -> ground rail
+Middle pin -> ground pin of octocoupler?
+Normally closed (NC) -> pin of smd switch
+
+Broken out micro usb:
+Ground pin -> ground rail
+5V pin -> 5V rail
+```
+
+As the micro usb port can be used to be power the esp8266 when the switch itself is powered by the active usb target itself, it needs to be broken out. Just use an old micro usb cable, clip it, strip the ends and use the
+- red wire for 4.7 volts
+- black wire for ground
+This way you dont need additional power.
+
+## software
+
+### wemos
+
+Clone the code and add the file `Settings.h` to the folder `include`. Then you have to fill in your correct parameters:
+
+```cpp
+#define WIFI_SSID ""                    //SSID of your wifi network
+#define WIFI_PSK ""                     //Preshared key of your wifi network
+#define MQTT_BROKER ""                  //IP address of your mqtt broker / homebridge
+#define MQTT_USER ""                    //Username for authenticating to mqtt broker
+#define MQTT_PASSWORD ""                //Password for authenticating to mqtt broker
+```
+
+After that you can build the code and flash it to your Wemos.
+
+### homebridge
+
+In Homebridge we need the plugin `homebridge-mqttthing`. The usb switch can then be attached with the following config:
+
+```json
+        {
+            "type": "switch",
+            "name": "USB target 1",
+            "username": "",
+            "password": "",
+            "logMqtt": true,
+            "topics": {
+                "getOn": "office/usb/target/1",
+                "setOn": "office/usb/target/1"
+            },
+            "integerValue": true,
+            "accessory": "mqttthing"
+        },
+        {
+            "type": "switch",
+            "name": "USB target 2",
+            "username": "",
+            "password": "",
+            "logMqtt": true,
+            "topics": {
+                "getOn": "office/usb/target/2",
+                "setOn": "office/usb/target/2"
+            },
+            "integerValue": true,
+            "accessory": "mqttthing"
+        }
+```
+
+Setting `logMqtt` to `true` is optional but logs the changed mqtt values to homebridge. Please adjust `username` and `password` to the correct values for your mqtt broker.
+
+## TODO
+
+- Add Fritzing
+- Add screenshots
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 #define RELAY D1
 #define ENABLE_DELAY 100
 #define MQTT_DELAY 100
-const char *topic_source_1 = "office/usb/source/1";
-const char *topic_source_2 = "office/usb/source/2";
+const char *topic_target_1 = "office/usb/target/1";
+const char *topic_target_2 = "office/usb/target/2";
 
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PSK;
@@ -18,8 +18,8 @@ const char *mqtt_password = MQTT_PASSWORD;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-// Default active USB source is 1
-int activeSource = 1;
+// Default active USB target is 1
+int activeTarget = 1;
 
 void switchRelay() {
   digitalWrite(RELAY, LOW);
@@ -28,57 +28,57 @@ void switchRelay() {
 }
 
 void publishTopics() {
-  if(activeSource == 1) {
-    // Source 1 is selected
-    client.publish(topic_source_1, "1");
-    client.publish(topic_source_2, "0");
-    Serial.println("+ Published active state source 1");
+  if(activeTarget == 1) {
+    // Target 1 is selected
+    client.publish(topic_target_1, "1");
+    client.publish(topic_target_2, "0");
+    Serial.println("+ Published active state target 1");
   } 
-  else if(activeSource == 2) {
-    // Source 2 is selected
-    client.publish(topic_source_1, "0");
-    client.publish(topic_source_2, "1");
-    Serial.println("+ Published active state source 2");
+  else if(activeTarget == 2) {
+    // Target 2 is selected
+    client.publish(topic_target_1, "0");
+    client.publish(topic_target_2, "1");
+    Serial.println("+ Published active state target 2");
   }
   else {
     // Invalid state
-    client.publish(topic_source_1, "0");
-    client.publish(topic_source_2, "0");
+    client.publish(topic_target_1, "0");
+    client.publish(topic_target_2, "0");
     Serial.println("+ Published panic mode");
   }
 }
 
-void switchUSBSource(char* source, String message, int switchDelay) {
-  if(strcmp(source, topic_source_1) == 0) {
-    // Operate on source 1
-    if(activeSource == 1 && message == "0") {
-      // Source 1 is currently active but should be switched off
+void switchUSBTarget(char* target, String message, int switchDelay) {
+  if(strcmp(target, topic_target_1) == 0) {
+    // Operate on target 1
+    if(activeTarget == 1 && message == "0") {
+      // Target 1 is currently active but should be switched off
       Serial.println("+ Switch from 1 to 2, 1 was active");
-      activeSource = 2;
+      activeTarget = 2;
       switchRelay();
       publishTopics();
     }
-    else if(activeSource == 2 && message == "1") {
-      // Source 1 is currently inactive but should be switched on
+    else if(activeTarget == 2 && message == "1") {
+      // Target 1 is currently inactive but should be switched on
       Serial.println("+ Switch from 2 to 1, 1 was inactive");
-      activeSource = 1;
+      activeTarget = 1;
       switchRelay();
       publishTopics();
     }
   }
-  else if(strcmp(source, topic_source_2) == 0) {
-    // Operate on source 2
-    if(activeSource == 2 && message == "0") {
-      // Source 2 is currently active but should be switched off
+  else if(strcmp(target, topic_target_2) == 0) {
+    // Operate on target 2
+    if(activeTarget == 2 && message == "0") {
+      // Target 2 is currently active but should be switched off
       Serial.println("+ Switch from 2 to 1, 2 was active");
-      activeSource = 1;
+      activeTarget = 1;
       switchRelay();
       publishTopics();
     }
-    else if(activeSource == 1 && message == "1") {
-      // Source 2 is currently inactive but should be switched on
+    else if(activeTarget == 1 && message == "1") {
+      // Target 2 is currently inactive but should be switched on
       Serial.println("+ Switch from 1 to 2, 2 was inactive");
-      activeSource = 2;
+      activeTarget = 2;
       switchRelay();
       publishTopics();
     }
@@ -91,7 +91,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
       char tmp = char(payload[i]);
       msg += tmp;
   }
-  switchUSBSource(topic, msg, ENABLE_DELAY);
+  switchUSBTarget(topic, msg, ENABLE_DELAY);
 }
 
 void setup() {
@@ -126,11 +126,11 @@ void loop() {
   if (!client.connected()) {
         while (!client.connected()) {
             client.connect("usb-switch-switcher", mqtt_user, mqtt_password);
-            client.subscribe(topic_source_1);
-            client.subscribe(topic_source_2);
+            client.subscribe(topic_target_1);
+            client.subscribe(topic_target_2);
             // Publish default state
-            client.publish(topic_source_1, "1");
-            client.publish(topic_source_2, "0");
+            client.publish(topic_target_1, "1");
+            client.publish(topic_target_2, "0");
             delay(MQTT_DELAY);
         }
     }
